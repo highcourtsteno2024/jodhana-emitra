0db0e8f4b20de1fbf67011c5aa513425789c144f
highcourtsteno2024 <highcourtsteno2024@gmail.com>
2026-09-07 16:14:02 +0530

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCrRPdpey7KfDU4A0NhC8VJ9M0uqyu5dOQ",
+  authDomain: "jodhana-emitra.firebaseapp.com",
+  projectId: "jodhana-emitra",
+  storageBucket: "jodhana-emitra.firebasestorage.app",
+  messagingSenderId: "297148194647",
+  appId: "1:297148194647:web:137469ce539a2d36fce812"
+}; if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig); } const db = firebase.firestore(); const storage = firebase.storage();
